7a0b4f81b44b7f3e3ff0b6ade7ce885ac2384cce
bud-caolibin <caolibin@budcreate.io>
2023-03-14 12:20:44 +0800

    fix:reactivephysics没有bind python

--- ctypes/add/main.py
+++ ctypes/add/main.py
@@ -3,6 +3,15 @@ import ctypes
 # 加载共享库
 mylib = ctypes.CDLL('./libmylib.so')
 
+# 定义函数签名和类型注释
+mylib.add.argtypes = (ctypes.c_int, ctypes.c_int)
+mylib.add.restype = ctypes.c_int
+
+
+# 定义Python函数，调用C++ DLL中的函数
+def add(x: int, y: int) -> int:
+    return mylib.add(x, y)
+
 
 # 调用共享库中的add函数
 result = mylib.add(2, 3)
--- ctypes/physics/main.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-rp3d = ctypes.CDLL("./libreactphysics3d.so")
-
-symbol_table = rp3d.__dict__
-
-for symbol in symbol_table:
-    print(symbol)
